d42f777310eb8f9b8210d9269e5327384c0e6c49
byllgrim <myemail>
2023-09-01 22:42:48 +0200

    fuck functional declarations

--- notep.c
+++ notep.c
@@ -8,23 +8,7 @@
 #include "config.h"
 
 
-enum unsaved_dialog {YES, NO, CANCEL};
-
-
-static GtkWidget  *create_font_chooser(void);
-static GtkWidget  *create_menu_bar(void);
-static GtkWidget  *create_text_view(void);
-static void        activate(GtkApplication *app);
-
-static int   save_activate(void);
-static int   saveas_activate(void);
-static void  open_activate(void);
-
-static gboolean  exit_notep(void);
-static int       save_file(void);
-static int       unsaved_dialog(void);
-static void      die(char *msg);
-static void      load_file(void);
+enum enum_unsaved_dialog {YES, NO, CANCEL};
 
 
 static GtkBuilder            *builder;
@@ -39,34 +23,188 @@ static char                  *filename = NULL;
 static int                    saved = 0;
 
 
-void
-activate(GtkApplication *app)
+int
+unsaved_dialog(void)
 {
-	GtkWidget *grid;
-	GtkWidget *menu_bar;
+	GtkWidget *dialog;
+	gint res;
 
-	builder = gtk_builder_new();
-	font_description = pango_font_description_from_string(default_font);
+	dialog = gtk_message_dialog_new(GTK_WINDOW(window),
+	                                GTK_DIALOG_MODAL,
+	                                GTK_MESSAGE_QUESTION,
+	                                GTK_BUTTONS_NONE,
+	                                "The document has been modified.\n"
+	                                "Save changes?");
+	gtk_dialog_add_buttons(GTK_DIALOG(dialog),
+	                       "Yes", YES,
+	                       "No", NO,
+	                       "Cancel", CANCEL, NULL);
 
-	window = gtk_application_window_new(app);
-	/* TODO GDK_KEY_PRESS_MASK and key-press-event */
-	gtk_window_set_title(GTK_WINDOW(window), "notep");
-	gtk_window_set_default_size(GTK_WINDOW(window), 320, 240);
-	g_signal_connect(window, "delete-event", G_CALLBACK(exit_notep), NULL);
+	res = gtk_dialog_run(GTK_DIALOG(dialog));
+	gtk_widget_destroy(dialog);
+	return res;
+}
 
-	grid = gtk_grid_new();
-	gtk_container_add(GTK_CONTAINER(window), grid);
+int
+save_file(void)
+{
+	FILE *file;
+	char *text;
+	GtkTextIter *start;
+	GtkTextIter *end;
 
-	menu_bar = create_menu_bar();
-	gtk_grid_attach(GTK_GRID(grid), menu_bar, 0, 0, 1, 1);
+	start = malloc(sizeof(GtkTextIter));
+	end = malloc(sizeof(GtkTextIter));
 
-	text_window = create_text_view(); /* TODO create_text_window ? */
-	gtk_grid_attach_next_to(GTK_GRID(grid), text_window, menu_bar,
-	                        GTK_POS_BOTTOM, 1, 1);
+	file = fopen(filename, "w");
+	gtk_text_buffer_get_start_iter(buffer, start);
+	gtk_text_buffer_get_end_iter(buffer, end);
+	text = gtk_text_buffer_get_text(buffer, start, end, TRUE);
+	fprintf(file, "%s", text);
 
-	/* buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view)); */
+	saved = 1;
+	gtk_text_buffer_set_modified(buffer, FALSE);
+	fclose(file);
+	return YES;
+}
 
-	gtk_widget_show_all(window);
+int
+saveas_activate(void)
+{
+	GtkWidget *dialog;
+	GtkFileChooser *chooser;
+	gint res;
+
+	dialog = gtk_file_chooser_dialog_new("Save file",
+	                                     GTK_WINDOW(window),
+	                                     GTK_FILE_CHOOSER_ACTION_SAVE,
+	                                     "_Cancel",
+	                                     GTK_RESPONSE_CANCEL,
+	                                     "_Save",
+	                                     GTK_RESPONSE_ACCEPT,
+	                                     NULL);
+
+	chooser = GTK_FILE_CHOOSER(dialog);
+	gtk_file_chooser_set_do_overwrite_confirmation(chooser, TRUE);
+
+	if (filename)
+		gtk_file_chooser_set_filename(chooser, filename);
+		/* TODO is this nescessary? */
+
+	res = gtk_dialog_run(GTK_DIALOG(dialog));
+	if (res == GTK_RESPONSE_ACCEPT) {
+		/* TODO g_free filename? */
+		filename = gtk_file_chooser_get_filename(chooser);
+		save_file();
+		res = YES;
+	} else {
+		res = CANCEL;
+	}
+
+	gtk_widget_destroy(dialog);
+	return res;
+}
+
+int
+save_activate(void)
+{
+	int res = NO;
+
+	if ((gtk_text_buffer_get_modified(buffer) == TRUE) || !saved)
+		res = saved ? save_file() : saveas_activate();
+
+	return res; /* TODO verify correctness of this return */
+}
+
+gboolean
+exit_notep(void)
+{
+	int prompt;
+
+	if (gtk_text_buffer_get_modified(buffer) == TRUE) {
+		prompt = unsaved_dialog();
+		if (prompt ==  YES)
+			prompt = save_activate();
+		if (prompt == CANCEL)
+			return TRUE;
+	}
+
+	exit(EXIT_SUCCESS);
+}
+
+void
+die(char *msg)
+{
+	GtkWidget *dialog;
+
+	dialog = gtk_message_dialog_new(GTK_WINDOW(window),
+	                                GTK_DIALOG_DESTROY_WITH_PARENT,
+	                                GTK_MESSAGE_ERROR,
+	                                GTK_BUTTONS_OK,
+	                                "%s", msg);
+	gtk_dialog_run(GTK_DIALOG(dialog));
+	gtk_widget_destroy(dialog);
+
+	fprintf(stderr, "%s\n", msg);
+	exit(EXIT_FAILURE);
+}
+
+void
+load_file(void)
+{
+	FILE *file;
+	size_t length;
+	char *text;
+
+	file = fopen(filename, "r");
+	fseek(file, 0, SEEK_END);
+	length = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	text = malloc((size_t)(length + 1)); /* TODO error checking */
+	if (fread(text, sizeof(char), length, file) != length)
+		die("notep: error reading file");
+	text[length] = '\0';
+	gtk_text_buffer_set_text(buffer, text, length);
+
+	saved = 1;
+	gtk_text_buffer_set_modified(buffer, FALSE);
+	fclose(file);
+}
+
+void
+open_activate(void)
+{
+	GtkWidget *file_chooser;
+	gint res;
+	int prompt;
+
+	if (gtk_text_buffer_get_modified(buffer) == TRUE) {
+		prompt = unsaved_dialog();
+		if (prompt ==  YES)
+			prompt = save_activate();
+		if (prompt == CANCEL)
+			return;
+		/* else 'button no' continues as normal */
+	}
+
+	file_chooser = gtk_file_chooser_dialog_new("Open file",
+	                                           GTK_WINDOW(window),
+	                                           GTK_FILE_CHOOSER_ACTION_OPEN,
+	                                           "_Cancel",
+	                                           GTK_RESPONSE_CANCEL,
+	                                           "_Open",
+	                                           GTK_RESPONSE_ACCEPT,
+	                                           NULL);
+	res = gtk_dialog_run(GTK_DIALOG(file_chooser));
+
+	if (res == GTK_RESPONSE_ACCEPT) {
+		filename = gtk_file_chooser_get_filename(
+			GTK_FILE_CHOOSER(file_chooser));
+		load_file();
+	}
+
+	gtk_widget_destroy(file_chooser);
 }
 
 GtkWidget *
@@ -76,7 +214,6 @@ create_menu_bar(void)
 	GtkWidget *open;
 	GtkWidget *save;
 	GtkWidget *saveas;
-	GtkWidget *font;
 
 	menu_bar = gtk_menu_bar_new();
 
@@ -115,6 +252,36 @@ create_text_view(void)
 	return scrolled_window;
 }
 
+void
+activate(GtkApplication *app)
+{
+	GtkWidget *grid;
+	GtkWidget *menu_bar;
+
+	builder = gtk_builder_new();
+	font_description = pango_font_description_from_string(default_font);
+
+	window = gtk_application_window_new(app);
+	/* TODO GDK_KEY_PRESS_MASK and key-press-event */
+	gtk_window_set_title(GTK_WINDOW(window), "notep");
+	gtk_window_set_default_size(GTK_WINDOW(window), 320, 240);
+	g_signal_connect(window, "delete-event", G_CALLBACK(exit_notep), NULL);
+
+	grid = gtk_grid_new();
+	gtk_container_add(GTK_CONTAINER(window), grid);
+
+	menu_bar = create_menu_bar();
+	gtk_grid_attach(GTK_GRID(grid), menu_bar, 0, 0, 1, 1);
+
+	text_window = create_text_view(); /* TODO create_text_window ? */
+	gtk_grid_attach_next_to(GTK_GRID(grid), text_window, menu_bar,
+	                        GTK_POS_BOTTOM, 1, 1);
+
+	/* buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view)); */
+
+	gtk_widget_show_all(window);
+}
+
 GtkWidget *
 create_font_chooser(void)
 {
@@ -122,89 +289,6 @@ create_font_chooser(void)
 	gtk_font_chooser_set_font_desc(GTK_FONT_CHOOSER(font_chooser), font_description);
 
 	return font_chooser;
-}
-
-void
-open_activate(void)
-{
-	GtkWidget *file_chooser;
-	gint res;
-	int prompt;
-
-	if (gtk_text_buffer_get_modified(buffer) == TRUE) {
-		prompt = unsaved_dialog();
-		if (prompt ==  YES)
-			prompt = save_activate();
-		if (prompt == CANCEL)
-			return;
-		/* else 'button no' continues as normal */
-	}
-
-	file_chooser = gtk_file_chooser_dialog_new("Open file",
-	                                           GTK_WINDOW(window),
-	                                           GTK_FILE_CHOOSER_ACTION_OPEN,
-	                                           "_Cancel",
-	                                           GTK_RESPONSE_CANCEL,
-	                                           "_Open",
-	                                           GTK_RESPONSE_ACCEPT,
-	                                           NULL);
-	res = gtk_dialog_run(GTK_DIALOG(file_chooser));
-
-	if (res == GTK_RESPONSE_ACCEPT) {
-		filename = gtk_file_chooser_get_filename(
-			GTK_FILE_CHOOSER(file_chooser));
-		load_file();
-	}
-
-	gtk_widget_destroy(file_chooser);
-}
-
-int
-save_activate(void)
-{
-	int res = NO;
-
-	if ((gtk_text_buffer_get_modified(buffer) == TRUE) || !saved)
-		res = saved ? save_file() : saveas_activate();
-
-	return res; /* TODO verify correctness of this return */
-}
-
-int
-saveas_activate(void)
-{
-	GtkWidget *dialog;
-	GtkFileChooser *chooser;
-	gint res;
-
-	dialog = gtk_file_chooser_dialog_new("Save file",
-	                                     GTK_WINDOW(window),
-	                                     GTK_FILE_CHOOSER_ACTION_SAVE,
-	                                     "_Cancel",
-	                                     GTK_RESPONSE_CANCEL,
-	                                     "_Save",
-	                                     GTK_RESPONSE_ACCEPT,
-	                                     NULL);
-
-	chooser = GTK_FILE_CHOOSER(dialog);
-	gtk_file_chooser_set_do_overwrite_confirmation(chooser, TRUE);
-
-	if (filename)
-		gtk_file_chooser_set_filename(chooser, filename);
-		/* TODO is this nescessary? */
-
-	res = gtk_dialog_run(GTK_DIALOG(dialog));
-	if (res == GTK_RESPONSE_ACCEPT) {
-		/* TODO g_free filename? */
-		filename = gtk_file_chooser_get_filename(chooser);
-		save_file();
-		res = YES;
-	} else {
-		res = CANCEL;
-	}
-
-	gtk_widget_destroy(dialog);
-	return res;
 }
 
 /* TODO fuck this bullshit
@@ -249,107 +333,6 @@ select_font(void)
 	);
 }
 */
-
-void
-load_file(void)
-{
-	FILE *file;
-	size_t length;
-	char *text;
-
-	file = fopen(filename, "r");
-	fseek(file, 0, SEEK_END);
-	length = ftell(file);
-	fseek(file, 0, SEEK_SET);
-
-	text = malloc((size_t)(length + 1)); /* TODO error checking */
-	if (fread(text, sizeof(char), length, file) != length)
-		die("notep: error reading file");
-	text[length] = '\0';
-	gtk_text_buffer_set_text(buffer, text, length);
-
-	saved = 1;
-	gtk_text_buffer_set_modified(buffer, FALSE);
-	fclose(file);
-}
-
-int
-save_file(void)
-{
-	FILE *file;
-	char *text;
-	GtkTextIter *start;
-	GtkTextIter *end;
-
-	start = malloc(sizeof(GtkTextIter));
-	end = malloc(sizeof(GtkTextIter));
-
-	file = fopen(filename, "w");
-	gtk_text_buffer_get_start_iter(buffer, start);
-	gtk_text_buffer_get_end_iter(buffer, end);
-	text = gtk_text_buffer_get_text(buffer, start, end, TRUE);
-	fprintf(file, "%s", text);
-
-	saved = 1;
-	gtk_text_buffer_set_modified(buffer, FALSE);
-	fclose(file);
-	return YES;
-}
-
-int
-unsaved_dialog(void)
-{
-	GtkWidget *dialog;
-	gint res;
-
-	dialog = gtk_message_dialog_new(GTK_WINDOW(window),
-	                                GTK_DIALOG_MODAL,
-	                                GTK_MESSAGE_QUESTION,
-	                                GTK_BUTTONS_NONE,
-	                                "The document has been modified.\n"
-	                                "Save changes?");
-	gtk_dialog_add_buttons(GTK_DIALOG(dialog),
-	                       "Yes", YES,
-	                       "No", NO,
-	                       "Cancel", CANCEL, NULL);
-
-	res = gtk_dialog_run(GTK_DIALOG(dialog));
-	gtk_widget_destroy(dialog);
-	return res;
-}
-
-gboolean
-exit_notep(void)
-{
-	int prompt;
-
-	if (gtk_text_buffer_get_modified(buffer) == TRUE) {
-		prompt = unsaved_dialog();
-		if (prompt ==  YES)
-			prompt = save_activate();
-		if (prompt == CANCEL)
-			return TRUE;
-	}
-
-	exit(EXIT_SUCCESS);
-}
-
-void
-die(char *msg)
-{
-	GtkWidget *dialog;
-
-	dialog = gtk_message_dialog_new(GTK_WINDOW(window),
-	                                GTK_DIALOG_DESTROY_WITH_PARENT,
-	                                GTK_MESSAGE_ERROR,
-	                                GTK_BUTTONS_OK,
-	                                "%s", msg);
-	gtk_dialog_run(GTK_DIALOG(dialog));
-	gtk_widget_destroy(dialog);
-
-	fprintf(stderr, "%s\n", msg);
-	exit(EXIT_FAILURE);
-}
 
 int
 main(int argc, char *argv[])
